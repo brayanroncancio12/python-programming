356420bcd63306e2dcbcf64d4c4ddaa3a86bce1b
brayanroncancio12 <rayanblack@hotmail.com>
2026-01-31 09:11:20 -0500

    Activity 1 solution

---
+++ activity_1.py
@@ -0,0 +1,18 @@
+a = 10
+b = 20
+c = 30
+
+d = a + b + c
+print('My name is Brayan Roncancio and my firts variable is:', a, 'my second variable is:',
+      b, 'my third variable is:', c, ' when we sum all the variables the variable together we get', d)
+
+x = 50
+y = 30
+
+print('The sum of x and y is:', x + y)
+print('The difference of x and y is:', x - y)
+print('The product of x and y is:', x * y)
+print('The division of x and y is:', x / y)
+print('The modulus of x and y is:', x % y)
+print('The exponentiation of x and y is:', x ** y)
+print('The floor division of x and y is:', x // y)
